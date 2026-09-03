cefd68e091e71e72bfc3463841f2863befe0ac5b
ronaldodfilho <ronaldodfilho@hotmail.com>
2026-09-03 16:07:33 -0300

    feat: adiciona modelos de dominio

---
+++ models.py
@@ -0,0 +1,22 @@
+﻿from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Alteracao:
+    registro_ans: str
+    razao_social: str
+    campo: str
+    valor_anterior: str
+    valor_atual: str
+
+    def eh_critica(self, campos_criticos: list) -> bool:
+        return self.campo in campos_criticos
+
+
+@dataclass
+class Inconsistencia:
+    registro_ans: str
+    razao_social: str
+    tipo: str
+    descricao: str
